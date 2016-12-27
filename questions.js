0e8daae43a1cc16aeae68ff14effa2a764fe613f
Dan Barker <drbarker@dstsystems.com>
2016-12-27 16:19:14 -0600

    Adding DevOps Trivia

---
+++ questions.js
@@ -0,0 +1,542 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_GB" : [
+        {
+            "Who was the founder of the DevOps K C Meetup?": [
+                "Aaron Blythe",
+                "Jerry Maguire",
+                "Dane Hammer",
+                "Chris Stein",
+                "Ryan McNair",
+                "Chase Pettet"
+            ]
+        },
+        {
+            "Which presenters first coined the term devops?": [
+                "Patrick Debois and Andrew Clay Shafer",
+                "Jez Humble and Dave Farley",
+                "Adrian Cockcroft and Brendan Gregg",
+                "Baron Schwartz and Anders Wallgren",
+                "Damon Edward and John Willis",
+                "Gene Kim and Kris Buytaert",
+                "John Allspaw and Martin Fowler"
+            ]
+        },
+        {
+            "Where was the first devops days held?": [
+                "Ghent, Belgium",
+                "Chicago, Illinois",
+                "Paris, France",
+                "Berlin, Germany",
+                "Minneapolis, Minnesota",
+                "San Francisco, California",
+                "Kansas City, Missouri"
+            ]
+        },
+        {
+            "What year was the first devops days?": [
+                "2009",
+                "2008",
+                "2010",
+                "2011",
+                "2007",
+                "2006"
+            ]
+        },
+        {
+            "What year was the devops k c meetup started?": [
+                "2013",
+                "2012",
+                "2011",
+                "2014",
+                "2015",
+                "2010",
+                "2009"
+            ]
+        },
+        {
+            "What year was the first devops days k c?": [
+                "2016",
+                "2015",
+                "2014",
+                "2013",
+                "2012",
+                "2011",
+                "2010",
+                "2009",
+                "2017"
+            ]
+        },
+        {
+            "Who was the first presenter at the devops k c meetup?": [
+                "Adam Jacob",
+                "Aaron Blythe",
+                "Andrew Clay Shaffer",
+                "Doug Cutting"
+            ]
+        },
+        {
+            "What day was the first devops k c meetup?": [
+                "April Fools\' Day",
+                "Saint Patrick\'s Day",
+                "Columbus Day",
+                "Valentine's Day",
+                "May Day",
+                "Ides of March"
+            ]
+        },
+        {
+            "Who hosts arrested devops?": [
+                "Matt Stratton, Trevor Hess, and Bridget Kromhout",
+                "Matt Stratton and Bridget Kromhout",
+                "John Willis and Damon Edwards",
+                "J. Paul Reed, Seth Thomas, and Pete Cheslock"
+            ]
+        },
+        {
+            "Who hosts devops cafe?": [
+              "John Willis and Damon Edwards",
+              "Matt Stratton, Trevor Hess, and Bridget Kromhout",
+              "Matt Stratton and Bridget Kromhout",
+              "J. Paul Reed, Seth Thomas, and Pete Cheslock"
+            ]
+        },
+        {
+            "Who hosts the ship show?": [
+              "J. Paul Reed, Seth Thomas, and Pete Cheslock",
+              "Matt Stratton, Trevor Hess, and Bridget Kromhout",
+              "Matt Stratton and Bridget Kromhout",
+              "John Willis and Damon Edwards"
+            ]
+        },
+        {
+            "In Chef, what is the fundamental element of configuration in an organization?": [
+                "Recipe",
+                "Cookbook",
+                "Role",
+                "Chef server",
+                "chef client"
+            ]
+        },
+        {
+            "Which of the following is Puppet’s cross-platform profiling library?": [
+                "Facter",
+                "Geppetto",
+                "Puppet D B",
+                "Hiera"
+            ]
+        },
+        {
+            "In what format are Ansible playbooks?": [
+                "yaml",
+                "json",
+                "xml",
+                "sdl"
+            ]
+        },
+        {
+            "Vagrantfile syntax is from which language?": [
+                "Ruby",
+                "Java",
+                "Python",
+                "Go",
+                "JavaScript",
+                "C"
+            ]
+        },
+        {
+            "Which of these is not a principle of DevOps?": [
+                "Rewarding firefighting",
+                "Resilient Engineering",
+                "Amplifying of feedback loops",
+                "Reduction of Tech Debt"
+            ]
+        },
+        {
+            "Which of these statements are true?": [
+                "All other answers"
+                "DevOps looks to extend Dev into Production",
+                "DevOps looks to embed Development into IT Operations",
+                "DevOps looks to embed IT Operations into Development"
+            ]
+        },
+        {
+            "Which phrase does DevOps accurately describe, encompass, embody?": [
+                "All other answers",
+                "DevOps is a cultural approach to improving communications between the development and operations teams in an organization",
+                "DevOps is the term describing someone who moderates the exchanges between development and operations",
+                "DevOps is the name of a job for an employee who can work as both a systems engineer and a developer"
+            ]
+        },
+        {
+            "The DevOps movement is an outgrowth of which software development methodology?": [
+                "Agile",
+                "Waterfall",
+                "Promise-based algorithms",
+                "Test-driven development and model-driven development"
+            ]
+        },
+        {
+            "Which of these tools is not associated with DevOps?": [
+                "Liebert M.P.X.",
+                "Chef",
+                "Puppet",
+                "Ansible"
+            ]
+        },
+        {
+            "The DevOps movement has evolved to solve which problem?": [
+                "All other answers",
+                "Increasingly complex, virtualized I.T. environments",
+                "The need for multiple rapidly timed software releases; sometimes many in one day",
+                "The traditional siloed approach to app development and deployment"
+            ]
+        },
+        {
+            "Which of these would not rank as a primary concern when choosing a DevOps tool?": [
+                "Integration with environmental sensors on the data center floor",
+                "Compatibility with cloud platforms, such as Amazon Web Services",
+                "Reporting and control integration with collaboration tools",
+                "Communication with other management or monitoring tools"
+            ]
+        },
+        {
+            "Where does Ansible come from?": [
+                "It's a fictional device capable of instantaneous or superluminal communication.",
+                "It's a fictional device used to communicate in multiple languages",
+                "It's a fictional device that builds robots in the epic thriller \"Ada, and the sons of Atlantis",
+                "It's a fictional character who battled culinary artisans and marionettes for supremacy over a great empire"
+            ]
+        },
+        {
+            "What does Kubernetes mean?": [
+                "Helmsman",
+                "Captain",
+                "Docker",
+                "Orchestrator"
+            ]
+        },
+        {
+            "Who created Kubernetes?": [
+                "Google",
+                "Red Hat",
+                "Amazon",
+                "Twitter"
+            ]
+        },
+        {
+            "Who created Mesos?": [
+                "Twitter",
+                "Google",
+                "Amazon",
+                "Facebook"
+            ]
+        },
+        {
+            "Hudson was forked, what's the name of the fork?": [
+                "Jenkins",
+                "Alfred",
+                "Watson",
+                "Jensen"
+            ]
+        },
+        {
+            "What was the term coined to describe actions taken through group text messaging platforms?": [
+                "chat ops",
+                "talk ops",
+                "slack ops",
+                "google ops"
+            ]
+        },
+        {
+            "Which is not a part of the ELK stack?": [
+                "Elastic Search",
+                "Log Stash",
+                "Kibana",
+                "logregator"
+            ]
+        },
+        {
+            "Which is not an Atlassian product?": [
+                "Remedy",
+                "JIRA",
+                "Confluence",
+                "Bamboo",
+                "crucible",
+                "hip chat"
+            ]
+        }
+    ],
+    "QUESTIONS_EN_US" : [
+        {
+            "Who was the founder of the DevOps K C Meetup?": [
+                "Aaron Blythe",
+                "Jerry Maguire",
+                "Dane Hammer",
+                "Chris Stein",
+                "Ryan McNair",
+                "Chase Pettet"
+            ]
+        },
+        {
+            "Which presenters first coined the term devops?": [
+                "Patrick Debois and Andrew Clay Shafer",
+                "Jez Humble and Dave Farley",
+                "Adrian Cockcroft and Brendan Gregg",
+                "Baron Schwartz and Anders Wallgren",
+                "Damon Edward and John Willis",
+                "Gene Kim and Kris Buytaert",
+                "John Allspaw and Martin Fowler"
+            ]
+        },
+        {
+            "Where was the first devops days held?": [
+                "Ghent, Belgium",
+                "Chicago, Illinois",
+                "Paris, France",
+                "Berlin, Germany",
+                "Minneapolis, Minnesota",
+                "San Francisco, California",
+                "Kansas City, Missouri"
+            ]
+        },
+        {
+            "What year was the first devops days?": [
+                "2009",
+                "2008",
+                "2010",
+                "2011",
+                "2007",
+                "2006"
+            ]
+        },
+        {
+            "What year was the devops k c meetup started?": [
+                "2013",
+                "2012",
+                "2011",
+                "2014",
+                "2015",
+                "2010",
+                "2009"
+            ]
+        },
+        {
+            "What year was the first devops days k c?": [
+                "2016",
+                "2015",
+                "2014",
+                "2013",
+                "2012",
+                "2011",
+                "2010",
+                "2009",
+                "2017"
+            ]
+        },
+        {
+            "Who was the first presenter at the devops k c meetup?": [
+                "Adam Jacob",
+                "Aaron Blythe",
+                "Andrew Clay Shaffer",
+                "Doug Cutting"
+            ]
+        },
+        {
+            "What day was the first devops k c meetup?": [
+                "April Fools\' Day",
+                "Saint Patrick\'s Day",
+                "Columbus Day",
+                "Valentine's Day",
+                "May Day",
+                "Ides of March"
+            ]
+        },
+        {
+            "Who hosts arrested devops?": [
+                "Matt Stratton, Trevor Hess, and Bridget Kromhout",
+                "Matt Stratton and Bridget Kromhout",
+                "John Willis and Damon Edwards",
+                "J. Paul Reed, Seth Thomas, and Pete Cheslock"
+            ]
+        },
+        {
+            "Who hosts devops cafe?": [
+                "John Willis and Damon Edwards",
+                "Matt Stratton, Trevor Hess, and Bridget Kromhout",
+                "Matt Stratton and Bridget Kromhout",
+                "J. Paul Reed, Seth Thomas, and Pete Cheslock"
+            ]
+        },
+        {
+            "Who hosts the ship show?": [
+                "J. Paul Reed, Seth Thomas, and Pete Cheslock",
+                "Matt Stratton, Trevor Hess, and Bridget Kromhout",
+                "Matt Stratton and Bridget Kromhout",
+                "John Willis and Damon Edwards"
+            ]
+        },
+        {
+            "In Chef, what is the fundamental element of configuration in an organization?": [
+                "Recipe",
+                "Cookbook",
+                "Role",
+                "Chef server",
+                "chef client"
+            ]
+        },
+        {
+            "Which of the following is Puppet’s cross-platform profiling library?": [
+                "Facter",
+                "Geppetto",
+                "Puppet D B",
+                "Hiera"
+            ]
+        },
+        {
+            "In what format are Ansible playbooks?": [
+                "yaml",
+                "json",
+                "xml",
+                "sdl"
+            ]
+        },
+        {
+            "Vagrantfile syntax is from which language?": [
+                "Ruby",
+                "Java",
+                "Python",
+                "Go",
+                "JavaScript",
+                "C"
+            ]
+        },
+        {
+            "Which of these is not a principle of DevOps?": [
+                "Rewarding firefighting",
+                "Resilient Engineering",
+                "Amplifying of feedback loops",
+                "Reduction of Tech Debt"
+            ]
+        },
+        {
+            "Which of these statements are true?": [
+                "All other answers"
+                "DevOps looks to extend Dev into Production",
+                "DevOps looks to embed Development into IT Operations",
+                "DevOps looks to embed IT Operations into Development"
+            ]
+        },
+        {
+            "Which phrase does DevOps accurately describe, encompass, embody?": [
+                "All other answers",
+                "DevOps is a cultural approach to improving communications between the development and operations teams in an organization",
+                "DevOps is the term describing someone who moderates the exchanges between development and operations",
+                "DevOps is the name of a job for an employee who can work as both a systems engineer and a developer"
+            ]
+        },
+        {
+            "The DevOps movement is an outgrowth of which software development methodology?": [
+                "Agile",
+                "Waterfall",
+                "Promise-based algorithms",
+                "Test-driven development and model-driven development"
+            ]
+        },
+        {
+            "Which of these tools is not associated with DevOps?": [
+                "Liebert M.P.X.",
+                "Chef",
+                "Puppet",
+                "Ansible"
+            ]
+        },
+        {
+            "The DevOps movement has evolved to solve which problem?": [
+                "All other answers",
+                "Increasingly complex, virtualized I.T. environments",
+                "The need for multiple rapidly timed software releases; sometimes many in one day",
+                "The traditional siloed approach to app development and deployment"
+            ]
+        },
+        {
+            "Which of these would not rank as a primary concern when choosing a DevOps tool?": [
+                "Integration with environmental sensors on the data center floor",
+                "Compatibility with cloud platforms, such as Amazon Web Services",
+                "Reporting and control integration with collaboration tools",
+                "Communication with other management or monitoring tools"
+            ]
+        },
+        {
+            "Where does Ansible come from?": [
+                "It's a fictional device capable of instantaneous or superluminal communication.",
+                "It's a fictional device used to communicate in multiple languages",
+                "It's a fictional device that builds robots in the epic thriller \"Ada, and the sons of Atlantis",
+                "It's a fictional character who battled culinary artisans and marionettes for supremacy over a great empire"
+            ]
+        },
+        {
+            "What does Kubernetes mean?": [
+                "Helmsman",
+                "Captain",
+                "Docker",
+                "Orchestrator"
+            ]
+        },
+        {
+            "Who created Kubernetes?": [
+                "Google",
+                "Red Hat",
+                "Amazon",
+                "Twitter"
+            ]
+        },
+        {
+            "Who created Mesos?": [
+                "Twitter",
+                "Google",
+                "Amazon",
+                "Facebook"
+            ]
+        },
+        {
+            "Hudson was forked, what's the name of the fork?": [
+                "Jenkins",
+                "Alfred",
+                "Watson",
+                "Jensen"
+            ]
+        },
+        {
+            "What was the term coined to describe actions taken through group text messaging platforms?": [
+                "chat ops",
+                "talk ops",
+                "slack ops",
+                "google ops"
+            ]
+        },
+        {
+            "Which is not a part of the ELK stack?": [
+                "Elastic Search",
+                "Log Stash",
+                "Kibana",
+                "logregator"
+            ]
+        },
+        {
+            "Which is not an Atlassian product?": [
+                "Remedy",
+                "JIRA",
+                "Confluence",
+                "Bamboo",
+                "crucible",
+                "hip chat"
+            ]
+        }
+    ]
+};
